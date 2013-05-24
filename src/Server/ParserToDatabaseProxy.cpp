118db30291f030f08955f2cdd2870475a3772ce2
fiedukow <fiedukow@gmail.com>
2013-05-24 22:02:41 +0200

    Added missing files. Sorry.

---
+++ src/Server/ParserToDatabaseProxy.cpp
@@ -0,0 +1,157 @@
+#include "ParserToDatabaseProxy.h"
+#include "Database.h"
+#include "Exceptions.h"
+#include "Common/FieldCondition.h"
+#include "Common/Field.h"
+#include <Parser/Parser.hpp>
+
+#include <cassert>
+
+namespace Server
+{
+
+ParserToDatabaseProxy::ParserToDatabaseProxy(Database& db)
+  : db_(db)
+{}
+
+void ParserToDatabaseProxy::handleOperation(OperationPtr operation)
+{
+  assert(operation.get() != NULL);
+  switch(operation->type)
+  {
+    case Operation::INPUT:
+    case Operation::READ:
+      handleSelectOperation(operation);
+      break;
+    case Operation::OUTPUT:
+      handleInsertOperation(operation);
+      break;
+  }
+}
+
+bool ParserToDatabaseProxy::shouldLastOperationWaiting() const
+{
+  return lastOperationWaiting_;
+}
+
+std::string ParserToDatabaseProxy::getLastOperationAnswer() const
+{
+  return lastOperationAnswer_;
+}
+
+void ParserToDatabaseProxy::handleSelectOperation(OperationPtr operation)
+{ 
+  Common::SelectDescription selectDesc = buildSelectDescription(operation);
+  Common::Tuple answer;
+
+  try
+  {
+    switch(operation->type)
+    {
+      case Operation::INPUT:
+        answer = db_.input(selectDesc);
+      break;
+      case Operation::READ:
+        answer = db_.read(selectDesc);
+      break;
+      default:
+        assert(false);
+    }
+  }
+  catch(Exceptions::TupleDoesNotExistException& e)
+  {
+    lastOperationWaiting_ = true;
+    lastOperationAnswer_ = "";
+    return;
+  }
+  
+  lastOperationAnswer_ = serializeAnswer(answer);
+  return;
+}
+
+void ParserToDatabaseProxy::handleInsertOperation(OperationPtr operation)
+{
+  Common::InsertDescription insertDesc = buildInsertDescription(operation);
+  db_.output(insertDesc);
+  lastOperationWaiting_ = false;
+  lastOperationAnswer_ = "ADDED TUPLE TO DATABASE";
+}
+
+Common::SelectDescription ParserToDatabaseProxy::buildSelectDescription(OperationPtr operation)
+{
+  Common::SelectDescription::FieldConditions conditions;
+  for(Element* element : *(operation->elements))
+  {
+    Common::FieldCondition* condition;
+    Common::FieldCondition::ConditionType type = getConditionType(element);
+    switch(element->type)
+    {
+      case Element::INT:
+        condition = new Common::IntFieldCondition(element->int_value, type);
+        break;
+      case Element::NUM:
+        condition = new Common::FloatFieldCondition(element->num_value, type);
+        break;
+      case Element::STR:
+        condition = new Common::StringFieldCondition(element->str_value, type);
+        break;
+    }
+    conditions.push_back(condition);
+  }
+
+  return Common::SelectDescription(conditions);
+}
+
+Common::FieldCondition::ConditionType ParserToDatabaseProxy::getConditionType(Element* element)
+{
+  switch(element->constraint)
+  {
+    case Element::ANY:
+      return Common::FieldCondition::Any;
+    case Element::LT:
+      return Common::FieldCondition::Less;
+    case Element::GT:
+      return Common::FieldCondition::Greater;      
+    case Element::EQ:
+      return Common::FieldCondition::Equal;
+  }
+}
+
+Common::InsertDescription ParserToDatabaseProxy::buildInsertDescription(OperationPtr operation)
+{
+  Common::Tuple tuple;
+  for(Element* element : *(operation->elements))
+  {
+    switch(element->type)
+    {
+      case Element::INT:
+        tuple.push_back(new Common::IntField(element->int_value));
+      break;
+      case Element::NUM:
+        tuple.push_back(new Common::FloatField(element->num_value));
+      break;
+      case Element::STR:
+        tuple.push_back(new Common::StringField(element->str_value));
+      break;
+    }
+  }
+  return Common::InsertDescription(tuple);
+}
+
+std::string ParserToDatabaseProxy::serializeAnswer(Common::Tuple tuple)
+{
+  std::string answer = "(";
+  bool first = true;
+  for(Common::Field* field : tuple)
+  {
+    if(!first)
+      answer += ", ";
+    else
+      first = false;
+    answer += field->asString();    
+  }
+  answer += ")";
+  return answer;
+}
+
+}//namespace Server
---
+++ src/Server/ParserToDatabaseProxy.h
@@ -0,0 +1,45 @@
+#ifndef _PARSER_TO_DATABASE_PROXY_
+#define _PARSER_TO_DATABASE_PROXY_
+#include <memory>
+#include "Common/QueryStructures.h"
+
+class Operation;
+class Element;
+typedef std::shared_ptr<Operation> OperationPtr;
+
+namespace Server
+{
+
+class Database;
+
+/**
+ * This class is only reentrant, use multiinstances in multithreading
+ */
+class ParserToDatabaseProxy
+{
+public:
+  ParserToDatabaseProxy(Database& db);
+
+  void handleOperation(OperationPtr operation);
+  bool shouldLastOperationWaiting() const;
+  std::string getLastOperationAnswer() const;
+
+private:
+  void handleSelectOperation(OperationPtr operation);
+  void handleInsertOperation(OperationPtr operation);
+
+  static Common::SelectDescription buildSelectDescription(OperationPtr operation);
+  static Common::InsertDescription buildInsertDescription(OperationPtr operation);
+  
+  static Common::FieldCondition::ConditionType getConditionType(Element* element);
+  static std::string serializeAnswer(Common::Tuple tuple);
+
+private:
+  Database& db_;
+  bool lastOperationWaiting_;
+  std::string lastOperationAnswer_;
+};
+
+}//namespace Server
+
+#endif 
